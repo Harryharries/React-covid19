5bc533056b9e14a4e9ede3a28042f7c119753d90
Harryharries <a1021604754@gmail.com>
2020-08-12 00:47:45 -0500

    fix return global bug

--- src/App.js
+++ src/App.js
@@ -1,77 +1,36 @@
-// import React from 'react';
-
-// import {Cards, Chart,CountryPicker} from './components';
-// import styles from './App.module.css'
-// import {fetchData} from './api'
-// class App extends React.Component{
-//     state = {
-//         data: {},
-//         country:''
-//     }
-
-//     async componentDidMount(){
-//         const fetchedData = await fetchData();
-
-//         this.setState({data: fetchedData});
-//     }
-
-//     handleCountryChange = async(country) =>{
-//         const data = await fetchData(country);
-//         //console.log(fetchedData);
-//         this.setState({data, country: country});
-//     }
-//     render() {
-//         const {data, country} = this.state;
-
-//         return(
-//             <div className={styles.container}>
-//                 <Cards data={data}/>
-//                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
-//                 <Chart data={data} country={country}/>
-//             </div>
-//         )   
-//     }
-// }
-
-// export default App;
-
 import React from 'react';
 
-import { Cards, CountryPicker, Chart } from './components';
-import { fetchData } from './api/';
-import styles from './App.module.css';
+import {Cards, Chart,CountryPicker} from './components';
+import styles from './App.module.css'
+import {fetchData} from './api'
+class App extends React.Component{
+    state = {
+        data: {},
+        country:''
+    }
 
+    async componentDidMount(){
+        const fetchedData = await fetchData();
 
-class App extends React.Component {
-  state = {
-    data: {},
-    country: '',
-  }
+        this.setState({data: fetchedData});
+    }
 
-  async componentDidMount() {
-    const data = await fetchData();
+    handleCountryChange = async(country) =>{
+        const data = await fetchData(country);
+        //console.log(fetchedData);
+        this.setState({data, country: country});
+    }
+    render() {
+        const {data, country} = this.state;
 
-    this.setState({ data });
-  }
-
-  handleCountryChange = async (country) => {
-    const data = await fetchData(country);
-
-    this.setState({ data, country: country });
-  }
-
-  render() {
-    const { data, country } = this.state;
-
-    return (
-      <div className={styles.container}>
-    
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} /> 
-      </div>
-    );
-  }
+        return(
+            <div className={styles.container}>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
+            </div>
+        )   
+    }
 }
 
 export default App;
